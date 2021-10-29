ad4cadf8546039c18c03747d055add281afdd0fd
rclank <53981655+rclank@users.noreply.github.com>
2021-10-29 00:59:38 -0400

    functional load feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -232,8 +232,71 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+    // get tasks from localStorage
+    tasks = localStorage.getItem("tasks");
+
+    if (tasks === null) {
+        tasks = [];
+        return false;
+    }
+
+    // convert tasks from string back to array of objects
+    tasks = JSON.parse(tasks);
+    console.log(tasks);
+
+    // iterate through tasks array and create task elements on the page
+    for (i = 0; i < tasks.length; i++) {
+        tasks[i].id = taskIdCounter;
+
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+        listItemEl.setAttribute("data-task-id", tasks[i].id);
+
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+        
+        listItemEl.appendChild(taskInfoEl);
+
+        var taskActionsEl = createTaskActions(tasks[i].id);
+
+        listItemEl.appendChild(taskActionsEl);
+
+        if (tasks[i].status === 'to do') {
+            listItemEl.querySelector("select[name='status-change']").selectIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        } else if (tasks[i].status === 'in progress') {
+            listItemEl.querySelector("select[name='status-change']").selectIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        } else if (tasks[i].status === 'completed') {
+            listItemEl.querySelector("select[name='status-change']").selectIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+
+        taskIdCounter++;
+        console.log(listItemEl);
+
+    //     let currentTaskObj = tasks[i];
+    //     let currentTaskEl = createTaskEl(currentTaskObj);
+    //     switch (currentTaskObj.status) {
+    //         case 'to do':
+    //             tasksToDoEl.appendChild(currentTaskEl);
+    //             break;
+    //         case 'in progress':
+    //             tasksInProgressEl.appendChild(currentTaskEl);
+    //             break;
+    //         case 'completed':
+    //             tasksCompletedEl.appendChild(currentTaskEl);
+    //             break;
+    //     }
+    }
+}
+
 formEl.addEventListener("submit", taskFormHandler);
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
